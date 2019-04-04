9f8181b0fe4029a7cc75ad6e82b4b91e2e5b830d
rushtongarth <stephen.garth@gmail.com>
2019-04-04 06:37:56 -0500

    updates to dup check

--- src/db/loaddigest.py
+++ src/db/loaddigest.py
@@ -4,22 +4,34 @@ import itertools as it
 import numpy as np
 import pandas as pd
 import sqlalchemy as sql
-from difflib import SequenceMatcher as seqm
-from string import punctuation as punct
+import difflib as diff
+import string
+
+punct = string.punctuation
+
 
 
 class LoadDigest(object):
   def __init__(self,digest):
     self.dd = digest
+    self.sm = diff.SequenceMatcher(lambda X: X in punct)
+  def __ratio(self,X,Y):
+    # diff function to calculate diffs
+    self.sm.set_seqs(X,Y)
+    return self.sm.ratio()
+  def __hasdups(self,df,col):
+    bools = df.duplicated(col,keep=False)
+    return bools.any()
   def prep(self):
     attr = ['date','body','link','shakey','title']
     ebase, arts = self.dd.as_dblist()
     art_attr = zip(*map(op.attrgetter(*attr),arts))
     art_dict = dict(zip(attr,art_attr))
-    df = pd.DataFrame.from_dict(art_dict)
-    dups = df[df.duplicated('shakey',keep=False)]
-    # now use sequence matcher from diff lib
-    #diff function
-    F = lambda Y: seqm(lambda X: X in punct,Y.title.upper(),Y.body).ratio()
-    # get ratios
-    dups = dups[dups.apply(F,axis=1)<0.6]
+    self.df = pd.DataFrame.from_dict(art_dict)
+    if self.__hasdups(self.df,'shakey'):
+      dups = self.df[self.df.duplicated('shakey',keep=False)].copy()
+      dups.update(dups.title.str.upper().str.replace(' ',''))
+      # get ratios and keep low values
+      #notclose=[
+      #  for e,(a,b) in dups[['date'
+      
