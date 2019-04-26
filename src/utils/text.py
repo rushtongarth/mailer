570df03c7aa116c6ed0a1a2dff5efb0415e1baac
rushtongarth <stephen.garth@gmail.com>
2019-04-26 06:29:18 -0500

    updates to preprocessing in text.py

--- src/utils/text.py
+++ src/utils/text.py
@@ -1,1 +1,23 @@
-import pandas as pd,torchtext,spacy,sqlalchemy as sql,sqlalchemy.orm as sqlorm,numpy as np
+import pandas as pd
+import torchtext
+import spacy
+import sqlalchemy as sql
+import sqlalchemy.orm as sqlorm
+import numpy as np
+from db.api import dbapi
+
+class Preprocess(object):
+  '''preprocess text data'''
+  def __init__(self,session,cols = None):
+    self.api = dbapi(session)
+    if cols is None:
+      cols = ['body','title']
+    data = self.api.as_df(*cols)
+    self.data = data.reindex(cols,axis=1)
+    
+  def __src_prep(self,col):
+    to_prep = getattr(self.data,col)
+    to_prep = to_prep.str.replace('\s{2,}',' ')
+    to_prep = to_prep.str.split(pat='. ',expand=True)
+    to_prep.fillna('<pad>',inplace=True)
+
