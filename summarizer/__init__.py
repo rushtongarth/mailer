967e3667f2dd815b768bf61f23fe8459d3138c38
rushtongarth <stephen.garth@gmail.com>
2019-05-13 06:26:29 -0500

    addition of encoder

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -1,1 +1,6 @@
 import torch
+
+if torch.cuda.is_available():
+    DEVICE = torch.device('cuda:0')
+else:
+    DEVICE = torch.device('cpu')
--- summarizer/encoder.py
+++ summarizer/encoder.py
@@ -2,26 +2,40 @@ from . import torch
 
 class Encoder(torch.nn.Module):
   
-  def __init__(self, input_size, hidden_size):
-    super(Encoder, self).__init__()
-    self.hidden_size = hidden_size
-    self.embedding = torch.nn.Embedding(input_size, hidden_size)
-    self.frwd_gru = torch.nn.GRU(hidden_size, hidden_size, batch_first=True)
-    self.bkwd_gru = torch.nn.GRU(hidden_size, hidden_size, batch_first=True)
-    self.out_cval = torch.nn.Linear(2 * self.hidden_size, self.hidden_size)
-    self.out_hval = torch.nn.Linear(2 * self.hidden_size, self.hidden_size)
-  def forward(self,frwd_vals,bkwd_vals):
-    bs,max_len,*_ = frwd_vals.size()
-    sz1 = bs, max_len, self.hidden_size * 2
-    frwd_emb = self.embedding(frwd_vals)
-    bkwd_emb = self.embedding(bkwd_vals)
-    mask = frwd_vals.eq(0).detach()
-    frwd_out,frwd_state = self.frwd_gru(frwd_emb)
-    bkwd_out,bkwd_state = self.bkwd_gru(bkwd_emb)
-    hidden_concat = torch.cat((frwd_out,bkwd_out),2)
-    inv_mask = mask.eq(0).unsqueeze(2)
-    inv_mask = inv_mask.expand(sz1).float().detach()
-    hidden_out = hidden_concat * inv_mask
-    final_hproj = self.out_hval(torch.cat((frwd_state[0].squeeze(0), bkwd_state[0].squeeze(0)), 1))
-    final_cproj = self.out_cval(torch.cat((frwd_state[1].squeeze(0), bkwd_state[1].squeeze(0)), 1))
-    return hidden_out, final_hproj, final_cproj, mask
+    def __init__(self, input_size, hidden_size, n_layers=1, dropout=0):
+        super(Encoder, self).__init__()
+        self.n_layers = n_layers
+        self.hidden_size = hidden_size
+        __drop = 0 if n_layers>1 else dropout
+        self.embedding = torch.nn.Embedding(input_size, hidden_size)
+        # Initialize GRU
+        self.rnn = torch.nn.GRU(
+            hidden_size, hidden_size, n_layers,
+            dropout = __drop,
+            batch_first = True,
+            bidirectional = True,
+        )
+        self.out_cval = torch.nn.Linear(2 * self.hidden_size, self.hidden_size)
+        self.out_hval = torch.nn.Linear(2 * self.hidden_size, self.hidden_size)
+
+    def forward(self, in_seqs, in_lens, hidden=None
+        embed = self.embedding(in_seqs)
+        # Pack padded batch of sequences for RNN module
+        packed = torch.nn.utils.rnn.pack_padded_sequence(
+            embed, in_lens, batch_first=True
+        )
+        e_out, e_hid = self.rnn(packed,hidden)
+        e_out, _ = torch.nn.utils.rnn.pad_packed_sequence(
+            e_out, batch_first=True
+        )
+        # e_out dim = bs, n_seq, 2*n_hid
+        e_out = e_out.contiguous()
+        # shape of e_h: 2, bs, n_hid
+        h, c = e_hid
+        # bs, 2*n_hid
+        h = torch.cat(list(h), dim=1)
+        c = torch.cat(list(c), dim=1)
+        # bs,n_hid
+        h_reduced = torch.nn.functional.relu(self.out_hval(h))
+        c_reduced = torch.nn.functional.relu(self.out_cval(c))
+        return e_out, (h_reduced, c_reduced)
