f2af83b55fc494ea0ad445d04e40b8ef63a3c1c7
rushtongarth <stephen.garth@gmail.com>
2019-07-11 06:32:25 -0500

    adding daily page builder;

--- src/sitebuild/daily.py
+++ src/sitebuild/daily.py
@@ -1,12 +1,11 @@
 import requests
-import pathlib
+import pprint
+import json
 from datetime import datetime, date
 
-from configparser import ConfigParser, ExtendedInterpolation
-    
 
 class DailyBuild(object):
-     '''daily site build 
+     '''daily site builder 
      '''
      
      day = {
@@ -22,27 +21,25 @@ class DailyBuild(object):
          self.dow = self._date.weekday()
          self.dow_verbose = self.day[self.dow]
          self.__config_load(config)
-         
      def __config_load(self,config):
-         P = pathlib.Path(config).resolve()
-         if not P.exists():
-             err_str = "The file: '{}' does not exist"
-             raise FileNotFoundError(err_str.format(P))
-         self.cfg = ConfigParser(
-             interpolation = ExtendedInterpolation()
-         )
-         with P.open() as f:
-             self.cfg.read_file(f)
-
-     def deploy(self,to_load):
-         url = self.cfg.get('app_page','apiurl')
-         payload = {
-             'action' : 'deploy',
-             'site' : self.cfg.get('DEFAULT','appsite'),
-             'key' : self.cfg.get('app_page','apikey'),
-             'resource': '{}.html'.format(self.dow_verbose),
-             'code':to_load
+         self.url = config.get('app_page','apiurl')
+         self.payload = {
+             'site' : config.get('DEFAULT','appsite'),
+             'key' : config.get('app_page','apikey'),
          }
-         return requests.post(url,data=payload).json()
+     def deploy(self,to_load):
+         payload = dict(
+             action = 'deploy',
+             resource = '{}.html'.format(self.dow_verbose),
+             code = to_load,
+             **self.payload
+         )
+         obj = requests.post(self.url,data=payload).json()
+         if any(obj.values()):
+             print("ERROR!")
+             pprint.pprint(obj,width=40,indent=2)
+         else:
+             pprint.pprint(obj,width=40,indent=2)
+         return obj
          
 
