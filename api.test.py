d32dc2abb2adb9e6542eb7727e7414208a2a570c
rushtongarth <stephen.garth@gmail.com>
2019-08-15 06:33:56 -0500

    initial test on gmail api

---
+++ api.test.py
@@ -0,0 +1,21 @@
+from googleapiclient.discovery import build
+import pickle as pkl
+
+def ListMessagesMatchingQuery(cred_file,qstr="no-reply@arxiv.org"):
+    
+    with open(cred_file,'r') as cf:
+        creds = pkl.load(cf)
+    
+    service = build('gmail', 'v1', credentials=creds)
+    M = service.users().messages()
+    L = m.list(userId="me",q=qstr).execute()
+    
+    messages = []
+    if 'messages' in L:
+      messages.extend(L['messages'])
+    while 'nextPageToken' in L:
+      page_token = L['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query, pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    return messages
