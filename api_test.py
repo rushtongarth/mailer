03b47b5e418820424431a8100c5f8bcccb7c4aa9
rushtongarth <stephen.garth@gmail.com>
2019-08-28 06:30:17 -0500

    updates and testing

--- api_test.py
+++ api_test.py
@@ -34,9 +34,15 @@ class MessageListing(object):
 #    def __init__(self,credentials,query="from:no-reply@arxiv.org"):
 
 
-#import base64
-#import email
+from googleapiclient.discovery import build
+import pickle as pkl, operator as op, numpy as np, base64, email
+#ML = MessageListing(c)
+#arr = ML.messages
+#mess = ML.msgs.get(userId='me',id=arr[0],format='raw').execute()
 #msg_str = base64.urlsafe_b64decode(mess['raw'].encode('ASCII'))
-#mime_msg = email.message_from_bytes(msg_str).as_string().splitlines()
-#X = np.where(np.char.startswith(email_arr,'arXiv'))[0]
-#sum(np.char.count(email_arr[slice(X[-1],-1)],'\\\\'))
+#email_arr = email.message_from_bytes(msg_str).as_string().splitlines()
+#repeat_mark = np.where(np.char.startswith(email_arr,'%%--%%--%%'))[0]
+#X = np.where(np.char.startswith(email_arr,'arXiv:'))[0]
+#X1 = X[X<loc1[0]]
+#Y = np.vstack((X1,np.concatenate((X1[1:],loc1)))).T
+#articles = [email_arr[slice(*row)] for row in Y]
