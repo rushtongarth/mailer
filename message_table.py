d0dc40833c343b0cee1a4c5e1f5a53dfdc3b9a73
rushtongarth <stephen.garth@gmail.com>
2019-09-18 06:26:51 -0500

    updating vocab builder

--- message_table.py
+++ message_table.py
@@ -2,9 +2,20 @@ import operator as op
 import numpy as np
 import pandas as pd
 from api_test import Message, MessageListing, Article
+import string
 
-ML = MessageListing('creds.pkl')
-arr = ML[-1]
-attrs = ['artid', 'authors', 'body', 'categories', 'link', 'title']
-get = op.attrgetter(*attrs)
-df = pd.DataFrame(map(get,arr.Articles),columns=attrs)
+def message_df(idx = 0, credentials = 'creds.pkl'):
+    ML = MessageListing(credentials)
+    arr = ML[idx]
+    attrs = ['artid', 'authors', 'body', 'categories', 'link', 'title']
+    get = op.attrgetter(*attrs)
+    df = pd.DataFrame(map(get,arr.Articles),columns=attrs)
+    return df
+
+def build_vocab(frame):
+    text = frane.body.explode()
+    text = text.str.replace(',','')
+    text = text.str.replace('-',' ')
+    text = text.str.replace('/',' ')
+    text = text.str.lower()
+    
