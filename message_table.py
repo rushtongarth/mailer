1535f107ede4c9f1456d9acf0e2c77daa0a80e7a
rushtongarth <stephen.garth@gmail.com>
2019-09-23 06:19:18 -0500

    update string functions

--- message_table.py
+++ message_table.py
@@ -2,7 +2,7 @@ import operator as op
 import numpy as np
 import pandas as pd
 from api_test import Message, MessageListing, Article
-from string import punctuation
+from string import punctuation as punct
 
 def message_df(idx = 0, credentials = 'creds.pkl'):
     ML = MessageListing(credentials)
@@ -12,8 +12,14 @@ def message_df(idx = 0, credentials = 'creds.pkl'):
     df = pd.DataFrame(map(get,arr.Articles),columns=attrs)
     return df
 
+def str_app(series,func,*args,**kwargs):
+    s1 = getattr(series,'str')
+    s2 = getattr(s1,func)
+    return s2(*args,**kwargs)
+
 def build_vocab(frame):
-    d = dict.fromkeys(punctuation,' ')
+    p = punct.replace('.','')
+    d = dict.fromkeys(p,' ')
     tr = str.maketrans(d)
     text = frame.body.explode()
     text = text.str.translate(tr)
@@ -21,5 +27,6 @@ def build_vocab(frame):
     text = text.str.lower()
     text = text.str.split().explode()
     return text
-    
-    
+
+
+
