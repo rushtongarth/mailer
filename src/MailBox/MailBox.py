71e89ed3557b01b4413013dfb761a3b1ad04261b
rushtongarth <stephen.garth@gmail.com>
2019-10-14 06:31:39 -0500

    more compliance changes

--- src/MailBox/MailBox.py
+++ src/MailBox/MailBox.py
@@ -1,13 +1,16 @@
 import abc
- 
+
 
 class AbstractMailBox(abc.ABC):
-  @abc.abstractmethod
-  def __init__(self,user,pswd,location):
-    pass
-  @abc.abstractmethod
-  def __enter__(self):
-    pass
-  @abc.abstractmethod
-  def __exit__(self,*args):
-    pass
+
+    @abc.abstractmethod
+    def __init__(self, user, pswd, location):
+        pass
+
+    @abc.abstractmethod
+    def __enter__(self):
+        pass
+
+    @abc.abstractmethod
+    def __exit__(self, *args):
+        pass
