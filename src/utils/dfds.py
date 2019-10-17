a72899b5cda44d89e1707095d38346f86c106252
rushtongarth <stephen.garth@gmail.com>
2019-10-17 05:59:51 -0500

    updates to dfds

--- src/utils/dfds.py
+++ src/utils/dfds.py
@@ -1,48 +1,57 @@
 import torchtext
 
 # inspired by:
-## https://github.com/pytorch/text/issues/488
+# https://github.com/pytorch/text/issues/488
+
 
 class DataFrameDataset(torchtext.data.Dataset):
-  """Class for using pandas DataFrames as a datasource"""
-  def __init__(self, examples, fields, filter_pred=None):
+    """Class for using pandas DataFrames as a datasource
+
+    Create a dataset from a pandas dataframe of
+    examples and fields
+
+    Parameters
+    ----------
+        :param examples: DataFrame of examples
+        :type examples: :class:`pandas.DataFrame`
+        :param fields: Fields to use, where keys are field names, and
+                       values are the associated fields.
+        :type fields: dict
+        :param filter_pred: use only exanples for which
+                            filter_pred(example) is true, or use all
+                            examples if None.
+        :type filter_pred: (callable or None)
     """
-    Create a dataset from a pandas dataframe of examples and Fields
-    Arguments:
-    
-    :param examples: DataFrame of examples
-    :type examples: :class:`pandas.DataFrame`
-    :param fields: Fields to use, where keys are field names, and values are the associated fields.
-    :type fields: dict
-    :param filter_pred: use only exanples for which filter_pred(example) is true, or use all examples if None.
-    :type filter_pred: (callable or None)
-    """
-    self.examples = examples.apply(SeriesExample.fromSeries, args=(fields,), axis=1).tolist()
-    if filter_pred is not None:
-      self.examples = list(filter(filter_pred, self.examples))
-    self.fields = dict(fields)
-    # Unpack field tuples
-    for n, f in self.fields.items():
-      if isinstance(n, tuple):
-        self.fields.update(zip(n, f))
-        del self.fields[n]
+    def __init__(self, examples, fields, filter_pred=None):
+        self.examples = examples.apply(
+            SeriesExample.fromSeries,
+            args=(fields,), axis=1).tolist()
+        if filter_pred is not None:
+            self.examples = list(filter(filter_pred, self.examples))
+        self.fields = dict(fields)
+        # Unpack field tuples
+        for n, f in self.fields.items():
+            if isinstance(n, tuple):
+                self.fields.update(zip(n, f))
+                del self.fields[n]
+
 
 class SeriesExample(torchtext.data.Example):
-  """Class to convert a pandas Series to an Example"""
-  @classmethod
-  def fromSeries(cls, data, fields):
-    return cls.fromdict(data.to_dict(), fields)
+    """Class to convert a pandas Series to an Example"""
 
-  @classmethod
-  def fromdict(cls, data, fields):
-    ex = cls()
+    @classmethod
+    def fromSeries(cls, data, fields):
+        return cls.fromdict(data.to_dict(), fields)
 
-    for key, field in fields.items():
-      if key not in data:
-        ostr = "Key '{}' was not found in input data"
-        raise ValueError(ostr.format(key))
-      if field is not None:
-        setattr(ex, key, field.preprocess(data[key]))
-      else:
-        setattr(ex, key, data[key])
-    return ex
+    @classmethod
+    def fromdict(cls, data, fields):
+        ex = cls()
+        for key, field in fields.items():
+            if key not in data:
+                ostr = "Key '{}' was not found in input data"
+                raise ValueError(ostr.format(key))
+            if field is not None:
+                setattr(ex, key, field.preprocess(data[key]))
+            else:
+                setattr(ex, key, data[key])
+        return ex
