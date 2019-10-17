99fedda76789e571c48ac80fe331a3407855cad3
rushtongarth <stephen.garth@gmail.com>
2019-10-17 06:03:49 -0500

    updates to init files

--- src/__init__.py
+++
@@ -1,5 +0,0 @@
-#import pkgutil
-
-#from src.MailBox.ReadMail import ReadMail
-#for loader, module_name, is_pkg in  pkgutil.walk_packages(__path__):
-#  print(loader, module_name, is_pkg)
--- src/db/__init__.py
+++ src/db/__init__.py
@@ -1,4 +1,3 @@
- 
 from .loader import DataBaser
 from .schema import EmailBase
 from .schema import ArticleBase
