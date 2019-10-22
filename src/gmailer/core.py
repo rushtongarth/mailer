a0576ed7009a7b6a8ee55314657eafc379200d93
rushtongarth <stephen.garth@gmail.com>
2019-10-22 06:32:10 -0500

    updates to gmailer core

--- src/gmailer/core.py
+++ src/gmailer/core.py
@@ -18,6 +18,16 @@ import operator as op
             
             #yield self.mlisting[i]
 
+def get_authors(listing_obj, idx=0):
+    D = co.defaultdict(set)
+    arts = listing_obj[idx].Articles
+    for x in arts:
+        for y in x.authors:
+            D[y] |= set(x.authors) - set([y])
+
+    return D
+
+
 def message_df(idx=0, credentials='creds.pkl'):
     ML = MessageListing(credentials)
     arr = ML[idx]
@@ -25,3 +35,22 @@ def message_df(idx=0, credentials='creds.pkl'):
     get = op.attrgetter(*attrs)
     df = pd.DataFrame(map(get, arr.Articles), columns=attrs)
     return df
+
+
+def str_app(series, func, *args, **kwargs):
+    s1 = getattr(series, 'str')
+    s2 = getattr(s1, func)
+    return s2(*args, **kwargs)
+
+
+def build_vocab(frame):
+    p = punct.replace('.', '')
+    d = dict.fromkeys(p, ' ')
+    tr = str.maketrans(d)
+    text = frame.body.explode()
+    text = str_app(text, 'translate', tr)
+    text = str_app(text, 'replace', '\s+', ' ', regex=True)
+    text = str_app(text, 'lower')
+    text = str_app(text, 'split').explode()
+    return text
+
