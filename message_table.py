08ae8ce1b4af346858686de31a3517e2c4b1200a
rushtongarth <stephen.garth@gmail.com>
2020-01-17 07:15:38 -0600

    bug found, issue was arXiv:2001.04887

--- message_table.py
+++ message_table.py
@@ -40,44 +40,51 @@ def vocab(frame):
     text = text[text != '']
     return text
 
-def debug(idx, creds):
+def debug(idx, creds, prepack=False):
     import pickle as pkl
     from googleapiclient.discovery import build
     from base64 import urlsafe_b64decode
     from email import message_from_bytes
 
+    # Get message ids
     qstr = " ".join([
         "from:no-reply@arxiv.org",
         "subject:(cs daily)",
     ])
-    qstr = kwargs.get('query', qstr)
     messages = []
     kw = dict(userId="me", q=qstr)
-    with open(credentials, 'rb') as f:
+    with open(creds, 'rb') as f:
         creds = pkl.load(f)
     service = build('gmail', 'v1', credentials=creds)
-    _msgs = service.users().messages()
-    msgs = _msgs.list(**kw).execute()
+    
+    service_messages = service.users().messages()
+    _msgs = service_messages.list(**kw)
+    msgs = _msgs.execute()
+    
     if 'messages' in msgs:
         messages.extend(msgs['messages'])
     while 'nextPageToken' in msgs:
         kw['pageToken'] = msgs['nextPageToken']
-        msgs = msgs.list(**kw).execute()
+        msgs = service_messages.list(**kw).execute()
         messages.extend(msgs['messages'])
     ids = map(op.itemgetter('id'), messages)
     mids = np.fromiter(ids, dtype=(str, 16))
-
-    m = msgs.get(id=mids[idx], userId="me", format='raw').execute()
-    mess = message_from_bytes(urlsafe_b64decode(m['raw'].encode('ASCII'))
+    # 
+    m = service_messages.get(id=mids[idx], userId="me", format='raw').execute()
+    mess = message_from_bytes(urlsafe_b64decode(m['raw'].encode('ASCII')))
     mess = np.array(mess.as_string().splitlines())
     _end = np.where(np.char.startswith(mess, '%%--%%--%%'))[0]
-    _art = np.where(np.char.startswith(mess, 'arXiv:'))[0]
+    trunc = mess[:_end[0]]
+    sw = np.where(np.char.startswith(trunc, 'arXiv:'))[0]
+    _art = sw[np.where(np.char.startswith(trunc[sw-1],'\\\\'))[0]]
+    #_art = np.where(np.char.startswith(mess, 'arXiv:'))[0]
     _art = _art[_art < _end[0]]
     slices = np.vstack((_art, np.concatenate((_art[1:], _end)))).T
+    if prepack:
+        return mess, slices
     Articles = np.empty(slices.shape[0], dtype=object)
     for e, s in enumerate(slices):
-        art = np.array([i for i in mess[slice(*s)] if len(i)])
-        Articles[e] = Article(art)
+        Articles[e] = np.array([i for i in mess[slice(*s)] if len(i)])
     return Articles
 
 
