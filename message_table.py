e7cfcfd0bf9e31fcbe00ea1b26c08f83f973fff3
rushtongarth <stephen.garth@gmail.com>
2020-02-13 07:11:26 -0600

    addition of test gmail test message sender function

--- message_table.py
+++ message_table.py
@@ -6,7 +6,10 @@ import numpy as np
 import pandas as pd
 from src.gmailer.listing.listing import MessageListing
 
+
 PandasDF = typing.TypeVar('pandas.core.frame.DataFrame')
+
+
 
 
 def get_authors(listing_obj, idx=0):
@@ -47,6 +50,21 @@ def googobj(creds='creds.pkl'):
     return build('gmail', 'v1', credentials=creds)
     
 
+def send_test_gmail(gobj,mess_to,mess_from):
+    from email.mime.text import MIMEText
+    import base64
+    message = MIMEText("<html><h1>Hello</h1><p>World</p></html>",'html')
+    message['to'] = mess_to
+    message['from'] = mess_from
+    message['subject'] = 'testing'
+    msg = {'raw': base64.urlsafe_b64encode(message.as_string()).decode()}
+    try:
+        tosend = gobj.users().messages().send(userId="me",body=msg).execute()
+        return tosend
+    except Exception as E:
+        raise E
+
+
 class Sanitizer(object):
     
     def __init__(self, fullframe, eos='@', punct=['.', '?', '!']):
@@ -55,20 +73,42 @@ class Sanitizer(object):
             str.maketrans(dict.fromkeys(punct, '')))
         __punct = dict.fromkeys(no_eos, ' ')
         self.punct_tr = str.maketrans(__punct)
-        self.eos_tr = str.maketrans(dict.fromkeys(punct,eos))
+        self.eos_tr = str.maketrans(dict.fromkeys(punct, eos))
         self.eos = eos
 
     def __vocab(self, series):
+        text = series.str.replace('e.g.', 'for example',regex=False)
+        text = series.str.replace('i.e.', 'that is',regex=False)
         text = series.str.translate(self.punct_tr)
         text = text.str.translate(self.eos_tr)
         text = text.str.replace('\s+', ' ', regex=True)
         text = text.str.replace('\ss\s', 's ', regex=True)
         text = text.str.replace('[0-9]', '#', regex=True)
         text = text.str.split(self.eos).explode().str.strip()
-        text = text[text != '']
-        return text
+        return text[text.str.len() > 0]
+        
+    def vocab(self, col1='body', col2='title', s_s='<s>', s_e='</s>'):
+        """Build a vocab as in get to the point."""
+        #self._word_to_id = {}
+        #self._id_to_word = {}
+        ## total number of words
+        #self._count = 0
+        ## tokens for input padding and out-of-vocab words
+        #PAD_TOK, UNK_TOK = '[PAD]', '[UNK]'
+        ## start/end of decoder input sequence
+        #START, END = '[START]', '[END]'
+        ## [UNK], [PAD], [START] and [STOP] get the ids 0,1,2,3.
+        #for w in [UNK_TOK, PAD_TOK, START, STOP]:
+            #self._word_to_id[w] = self._count
+            #self._id_to_word[self._count] = w
+            #self._count += 1
+        
+        #txt = self.__vocab(self.df.get(col1), s_s, s_e)
+        # pad the encoder input, decoder input and target sequence
+        return s_s + self.__vocab(self.df.get(col1)) + s_e
         
         
+
 
 
 
