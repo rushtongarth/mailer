ca4955d47e87bb08c7b88b73257d386dc47540f0
rushtongarth <stephen.garth@gmail.com>
2020-02-21 07:12:20 -0600

    updating sanitizer

--- message_table.py
+++ message_table.py
@@ -4,12 +4,12 @@ import collections as co
 from string import punctuation
 import numpy as np
 import pandas as pd
+import pickle as pkl
+from googleapiclient.discovery import build
 from src.gmailer.listing.listing import MessageListing
 
 
 PandasDF = typing.TypeVar('pandas.core.frame.DataFrame')
-
-
 
 
 def get_authors(listing_obj, idx=0):
@@ -26,30 +26,12 @@ def message_df(idx=0, credentials='creds.pkl'):
     arr = ML[idx]
     attrs = ['artid', 'authors', 'body', 'categories', 'link', 'title']
     get = op.attrgetter(*attrs)
-    df = pd.DataFrame(map(get, arr.Articles), columns=attrs)
-    return df
+    return pd.DataFrame(map(get, arr.Articles), columns=attrs)
 
 
-def vocab(frame):
-    p = punct.replace('.', '').replace('!', '').replace('?', '')
-    tr = str.maketrans(dict.fromkeys(p, ' '))
-    trans = frame.body.str.translate(tr)
-    text = trans.str.replace(r'[.!?]', '@', regex=True)
-    text = text.str.replace('\s+', ' ', regex=True)
-    text = text.str.replace('\ss\s', 's ', regex=True)
-    text = text.str.replace('[0-9]', '#', regex=True)
-    text = text.str.split('@').explode().str.strip()
-    text = text[text != '']
-    return text
-
-def googobj(creds='creds.pkl'):
-    import pickle as pkl
-    from googleapiclient.discovery import build
-    with open(creds, 'rb') as f:
-        creds = pkl.load(f)
-    return build('gmail', 'v1', credentials=creds)
-    
-
+def str_apply(ser,*args,**kwargs):
+    fn = op.methodcaller(*args, **kwargs)
+    return fn(getattr(ser,'str'))
 
 class Sanitizer(object):
     
@@ -62,36 +44,25 @@ class Sanitizer(object):
         self.eos_tr = str.maketrans(dict.fromkeys(punct, eos))
         self.eos = eos
 
-    def __vocab(self, series):
-        text = series.str.replace('e\.g\.', 'for example')
-        text = series.str.replace('i\.e\.', 'that is')
-        text = series.str.translate(self.punct_tr)
-        text = text.str.translate(self.eos_tr)
-        text = text.str.replace('\s+', ' ', regex=True)
-        text = text.str.replace('\ss\s', 's ', regex=True)
-        text = text.str.replace('[0-9]', '#', regex=True)
-        text = text.str.split(self.eos).explode().str.strip()
+    def __vocab(self, column):
+        series = self.df.get(column)
+        series = (
+            series.pipe(str_apply, 'replace', 'e\.\s*g\.', 'for example')
+                  .pipe(str_apply, 'replace', 'i\.\s*e\.', 'that is')
+                  .pipe(str_apply, 'translate', self.punct_tr)
+                  .pipe(str_apply, 'translate', self.eos_tr)
+                  .pipe(str_apply, 'replace', '\s+', ' ')
+                  .pipe(str_apply, 'replace', '\ss\s', 's ')
+                  .pipe(str_apply, 'replace', '[0-9]', '#')
+                  .pipe(str_apply, 'split', self.eos)
+        )
+        text = series.explode().str.strip()
         return text[text.str.len() > 0]
         
-    def vocab(self, col1='body', col2='title', s_s='<s>', s_e='</s>'):
+    def vocab(self, column_name):
         """Build a vocab as in get to the point."""
-        #self._word_to_id = {}
-        #self._id_to_word = {}
-        ## total number of words
-        #self._count = 0
-        ## tokens for input padding and out-of-vocab words
-        #PAD_TOK, UNK_TOK = '[PAD]', '[UNK]'
-        ## start/end of decoder input sequence
-        #START, END = '[START]', '[END]'
-        ## [UNK], [PAD], [START] and [STOP] get the ids 0,1,2,3.
-        #for w in [UNK_TOK, PAD_TOK, START, STOP]:
-            #self._word_to_id[w] = self._count
-            #self._id_to_word[self._count] = w
-            #self._count += 1
-        
-        #txt = self.__vocab(self.df.get(col1), s_s, s_e)
-        # pad the encoder input, decoder input and target sequence
-        return s_s + self.__vocab(self.df.get(col1)) + s_e
+        series = self.__vocab(column_name)
+        return series
         
         
 
